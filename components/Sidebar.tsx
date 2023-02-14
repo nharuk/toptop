a5bb2265878d3a9ceb995d43449faf4033aa9e84
haru.k.94 <nguyenkhanh.spr@gmail.com>
2023-02-14 21:00:06 +0700

    first commit

---
+++ components/Sidebar.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+import { AiFillHome, AiOutlineMenu } from 'react-icons/ai';
+import { ImCancelCircle } from 'react-icons/im';
+import Discover from './Discover';
+import SuggestedAccounts from './SuggestedAccounts';
+import Footer from './Footer';
+import useAuthStore from '../store/authStore';
+
+
+const Sidebar = () => {
+  const [showSidebar, setShowSidebar] = useState<Boolean>(true);
+  const { fetchAllUsers, allUsers }: any = useAuthStore();
+
+  return (
+    <div>
+      <div
+        className='block xl:hidden m-2 ml-4 mt-3 text-xl'
+        onClick={() => setShowSidebar(!showSidebar)}
+      >
+        {showSidebar ? <ImCancelCircle /> : <AiOutlineMenu />}
+      </div>
+      {showSidebar && (
+        <div className='xl:w-400 w-20 flex flex-col justify-start mb-10 border-r-2 border-gray-100 xl:border-0 p-3 '>
+          <div className='xl:border-b-2 border-gray-200 xl:pb-4'>
+            <Link href='/'>
+              <div 
+              // className={pathname === '/' ? activeLink : normalLink}
+              >
+                <p className='text-2xl'>
+                  <AiFillHome />
+                </p>
+                <span className='capitalize text-xl hidden xl:block'>
+                  For You
+                </span>
+              </div>
+            </Link>
+
+          </div>
+          <Discover />
+
+        </div>
+        )}
+      <SuggestedAccounts
+        fetchAllUsers={fetchAllUsers}
+        allUsers={allUsers}
+      />
+      <Footer />
+    </div>
+  )
+}
+
+export default Sidebar
--- next.config.js
+++ next.config.js
@@ -2,6 +2,22 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: [
+      'lh3.googleusercontent.com',
+      'cdn.pixabay.com',
+      'p16-amd-va.tiktokcdn.com',
+      'image.shutterstock.com',
+      'i.pinimg.com',
+      'kenh14cdn.com',
+      "",
+      'static2-images.vnncdn.net',
+      'www.thetimes.co.uk',
+      'www.vpopwire.com',
+      'images.wsj.net',
+      'www.planetsport.com'
+    ],
+  },
 }
 
 module.exports = nextConfig
